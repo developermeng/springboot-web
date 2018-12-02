3c2d6b4cc5f923bd82ed2dc055b3bef4abbd793f
meng_PC <1601973251@qq.com>
2018-12-02 21:04:07 +0800

    SpringTest

--- src/main/java/com/mengtest/springboot/SpringbootWebApplication.java
+++ src/main/java/com/mengtest/springboot/SpringbootWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//Test aa
+//Test aab
 @SpringBootApplication
 public class SpringbootWebApplication {
 
